e480287cb96f9060fa18043582cbb61d27a384b3
Prabhat Pundeer <prabhat12pundeer@gmail.com>
2024-03-11 14:51:00 +0530

    social- media half done

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Layout/>,
+    children:[
+      {
+        path:"",
+        element: <Home/>
+      },
+      {
+        path:"",
+        element: <About/>
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </React.StrictMode>,
 )
